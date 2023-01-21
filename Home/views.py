861530de40f4ad82a69ddb37e904001c9c77dc41
iamkathan <shethkathan4@gmail.com>
2023-01-21 06:41:41 +0000

    changes has been done

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse("How are you doing")
     return render(request, 'restaurant/index.html')
+
